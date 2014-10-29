eb10ccd05b327877d7f6b52d1e9f0d349f445cb6
Stephen Giles <s.giles@hotmail.co.uk>
2014-10-29 14:00:03 +0000

    Functionality improving. A username can be entered (although not yet insisted upon). User's own messages are not displayed using the socket, they go straight to the screen. When a user leaves the room is told who has left. When a message is posted you can see who posted it.

--- index.js
+++ index.js
@@ -8,17 +8,25 @@ app.get('/', function(request, response){
 		response.sendFile(__dirname + '/index.html');
 	});
 
+var storeUsername;
+
 io.on('connection', function(socket){
 	console.log('a user connected');
-	io.emit('new user entered', 'a new user has entered the arena - play nice');
+	socket.broadcast.emit('new user entered', 'a new user has entered the arena - play nice');
+
+	socket.on('username', function(username){
+		storeUsername = username;												
+	});
+
 	socket.on('disconnect', function() {
 		console.log('user disconnected');
+		socket.broadcast.emit('user left', storeUsername+' has left us');
 	});
 });
 
 io.on('connection', function(socket){
 	socket.on('chat message', function(msg){
-	io.emit('chat message', msg);													// removed console.log('message: ' + msg);
+	socket.broadcast.emit('chat message', msg, storeUsername);
 	});
 });
 
