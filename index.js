4e8de09a1002fe374e43c05661777fb72df26f85
Stephen Giles <s.giles@hotmail.co.uk>
2014-10-30 15:51:29 +0000

    Refactoring. Using an array to store users and the sockets they are using. This is helping to fix bugs for users leaving.

--- index.js
+++ index.js
@@ -9,18 +9,31 @@ app.get('/', function(request, response){
 	});
 
 var storeUsername;
+var usersConnected = []
 
 io.on('connection', function(socket){
 	console.log('a user connected');
 	socket.broadcast.emit('new user entered', 'a new user has entered the arena - play nice');
+	usersConnected.push(socket)
+	console.log(usersConnected.length)
 
 	socket.on('username', function(username){
-		storeUsername = username;												
+		socket._events['username'] = username;
+		//console.log("let's see if username is updated:", socket)												
 	});
 
-	socket.on('disconnect', function() {
-		console.log('user disconnected');
-		socket.broadcast.emit('user left', storeUsername+' has left us');
+	socket.on('disconnect', function(username) {
+		//console.log('user disconnected');
+		//i = usersConnected.indexOf(socket)
+		//console.log("the position of the user in the array was, ", i)
+		//console.log(socket)
+		console.log("test", socket)
+		if(socket._events['username'] === ''){
+			socket._events['username'] = "Mr. X";
+			console.log(socket._events['username'])
+
+		}
+		socket.broadcast.emit('user left', socket._events['username']+' has left us');
 	});
 });
 
